af7219f0412bdfb2d3adad681d6ee8c361e035fa
artur12313 <arturpisula11@wp.pl>
2022-05-19 14:37:34 +0200

    Test styling main layout and added functionality in passwordUpdate function

--- src/components/PasswordUpdate.js
+++ src/components/PasswordUpdate.js
@@ -6,10 +6,11 @@ class PasswordUpdate extends Component
     constructor(props){
         super(props);
         this.state = {
-            oldPassword: '',
+            currentPassword: '',
             newPassword: '',
             confirmNewPassword: '',
             isLoading: false,
+            msg: '',
             userId: ''
         };
     }
@@ -26,8 +27,8 @@ class PasswordUpdate extends Component
         this.setState({isLoading: true});
         const user = JSON.parse(localStorage.getItem("userData"));
         
-        await axios.post('#', {
-            oldPassword: this.state.oldPassword,
+        await axios.post('http://127.0.0.1:8000/api/password-update', {
+            currentPassword: this.state.currentPassword,
             newPassword: this.state.newPassword,
             confirmNewPassword: this.state.confirmNewPassword,
             userId: user.id
@@ -35,24 +36,44 @@ class PasswordUpdate extends Component
             this.setState({isLoading: true});
             if(response.data.status === 200)
             {
-
+                this.setState({
+                    msg: response.data.message,
+                });
             }
+            if(response.data.status === "failed" && response.data.success === undefined)
+            {
+                this.setState({
+                    msg: response.data.message,
+                });
+                setTimeout(() => {
+                    this.setState({msg: ''});
+                }, 2000);
+            }
+            else if (response.data.status === 'failed' && response.data.success === false)
+            {
+                this.setState({msg: response.data.message,});
+                setTimeout(() => {
+                    this.setState({msg: ''});
+                }, 2000);
+            }
+        }).catch((error) => {
+            console.log(error);
         })
     }
     render(){
         const isLoading = this.state.isLoading;
         return(
             <div className="PasswordUpdate d-flex">
-                <div className="col-md-4 p-2 info">
+                <div className="col-md-7 p-2 info">
                     <h3>Aktualizacja Hasła</h3>
                     <p>Upewnij się, że Twoje konto używa długiego, losowego hasła, aby zachować bezpieczeństwo.</p>
                 </div>
-                <div className="col-md-7 box">
+                <div className="col-md-9 box">
                     <form onSubmit={this.updatePassword}>
                         <div className="form-body p-4">
                             <div className="form-group col-md-6 mb-4">
                                 <label>Aktualne hasło:</label>
-                                <input type="password" className="form-control" name="oldPassword" onChange={this.handleInput} value={this.state.oldPassword} required></input>
+                                <input type="password" className="form-control" name="currentPassword" onChange={this.handleInput} value={this.state.currentPassword} required></input>
                             </div>
                             <div className="form-group col-md-6 mb-4">
                                 <label>Nowe hasło:</label>
